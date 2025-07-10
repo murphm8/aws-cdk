99cee569f97c058863100465a92cb55eaec545ae
Matthew Murphy <matmur@amazon.com>
2025-07-10 15:07:19 -0700

    Add AWS PCS L2 constructs

--- packages/aws-cdk-lib/aws-pcs/README.md
+++ packages/aws-cdk-lib/aws-pcs/README.md
@@ -1,4 +1,4 @@
-# AWS::PCS Construct Library
+# AWS Parallel Computing Service Construct Library
 <!--BEGIN STABILITY BANNER-->
 
 ---
@@ -19,21 +19,122 @@ This module is part of the [AWS Cloud Development Kit](https://github.com/aws/aw
 import * as pcs from 'aws-cdk-lib/aws-pcs';
 ```
 
-<!--BEGIN CFNONLY DISCLAIMER-->
+## Overview
 
-There are no official hand-written ([L2](https://docs.aws.amazon.com/cdk/latest/guide/constructs.html#constructs_lib)) constructs for this service yet. Here are some suggestions on how to proceed:
+AWS Parallel Computing Service (PCS) provides managed high-performance computing (HPC) resources using the Slurm scheduler. This module provides both low-level CloudFormation constructs (L1) and higher-level abstractions (L2) for working with PCS resources.
 
-- Search [Construct Hub for PCS construct libraries](https://constructs.dev/search?q=pcs)
-- Use the automatically generated [L1](https://docs.aws.amazon.com/cdk/latest/guide/constructs.html#constructs_l1_using) constructs, in the same way you would use [the CloudFormation AWS::PCS resources](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/AWS_PCS.html) directly.
+### AWS PCS Components
 
+- **Cluster**: The core HPC cluster with networking, scheduler, and Slurm configuration
+- **ComputeNodeGroup**: Scalable compute nodes with instance configurations and launch templates
+- **Queue**: Job queues that assign work to compute node groups
 
-<!--BEGIN CFNONLY DISCLAIMER-->
+## L2 Constructs
 
-There are no hand-written ([L2](https://docs.aws.amazon.com/cdk/latest/guide/constructs.html#constructs_lib)) constructs for this service yet. 
-However, you can still use the automatically generated [L1](https://docs.aws.amazon.com/cdk/latest/guide/constructs.html#constructs_l1_using) constructs, and use this service exactly as you would using CloudFormation directly.
+### Basic Usage
 
-For more information on the resources and properties available for this service, see the [CloudFormation documentation for AWS::PCS](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/AWS_PCS.html).
+```typescript
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as pcs from 'aws-cdk-lib/aws-pcs';
 
-(Read the [CDK Contributing Guide](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and submit an RFC if you are interested in contributing to this construct library.)
+// Create VPC for the cluster
+const vpc = new ec2.Vpc(this, 'HpcVpc', {
+  maxAzs: 2,
+  natGateways: 1,
+});
 
-<!--END CFNONLY DISCLAIMER-->
+// Create the PCS cluster
+const cluster = new pcs.Cluster(this, 'HpcCluster', {
+  clusterName: 'my-hpc-cluster',
+  vpc: vpc,
+  size: pcs.ClusterSize.SMALL,
+  scheduler: {
+    type: pcs.SchedulerType.SLURM,
+    version: '23.11',
+  },
+});
+
+// Create a launch template for compute nodes
+const launchTemplate = pcs.ComputeNodeGroup.createBasicLaunchTemplate(this, 'LaunchTemplate', {
+  vpc: vpc,
+  instanceType: ec2.InstanceType.of(ec2.InstanceClass.C5, ec2.InstanceSize.XLARGE),
+});
+
+// Create compute node group
+const computeNodeGroup = new pcs.ComputeNodeGroup(this, 'ComputeNodes', {
+  cluster: cluster,
+  launchTemplate: {
+    launchTemplate: launchTemplate,
+  },
+  scalingConfiguration: {
+    minInstanceCount: 0,
+    maxInstanceCount: 10,
+  },
+});
+
+// Create a queue to manage jobs
+const queue = new pcs.Queue(this, 'JobQueue', {
+  cluster: cluster,
+  computeNodeGroupConfigurations: [
+    { computeNodeGroup: computeNodeGroup },
+  ],
+});
+```
+
+### Advanced Configuration
+
+```typescript
+// Advanced Slurm configuration
+const advancedCluster = new pcs.Cluster(this, 'AdvancedCluster', {
+  vpc: vpc,
+  size: pcs.ClusterSize.LARGE,
+  slurmConfiguration: {
+    accounting: pcs.SlurmConfiguration.standardAccounting(30),
+    scaleDownIdleTimeInSeconds: 300,
+    customSettings: pcs.SlurmConfiguration.commonHpcSettings(),
+  },
+});
+
+// Spot instance compute group
+const spotComputeGroup = new pcs.ComputeNodeGroup(this, 'SpotCompute', {
+  cluster: advancedCluster,
+  launchTemplate: { launchTemplate: launchTemplate },
+  purchaseOption: pcs.PurchaseOption.SPOT,
+  spotConfiguration: {
+    allocationStrategy: pcs.SpotAllocationStrategy.CAPACITY_OPTIMIZED,
+  },
+  scalingConfiguration: {
+    minInstanceCount: 0,
+    maxInstanceCount: 100,
+  },
+});
+```
+
+### Key Features
+
+- **Sensible Defaults**: Minimal configuration required for common HPC use cases
+- **Slurm Integration**: Built-in support for Slurm accounting and custom settings
+- **Cost Optimization**: Spot instance support and auto-scaling capabilities
+- **Security**: Automatic security group and IAM role creation
+- **Import Support**: Import existing PCS resources using ARN or ID
+
+### Available Constructs
+
+#### L2 Constructs
+- `Cluster` - High-level cluster construct with sensible defaults
+- `ComputeNodeGroup` - Managed compute resources with scaling configuration
+- `Queue` - Job queue management with compute node group associations
+
+#### Helper Classes
+- `SlurmConfiguration` - Builder for Slurm settings
+- Enums for cluster sizes, purchase options, and allocation strategies
+
+## L1 Constructs
+
+You can also use the automatically generated L1 constructs, which correspond directly to CloudFormation resources:
+
+- `CfnCluster` - AWS::PCS::Cluster
+- `CfnComputeNodeGroup` - AWS::PCS::ComputeNodeGroup
+- `CfnQueue` - AWS::PCS::Queue
+
+For more information on the resources and properties available, see the [CloudFormation documentation for AWS::PCS](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/AWS_PCS.html).
---
+++ packages/aws-cdk-lib/aws-pcs/lib/queue.ts
@@ -0,0 +1,247 @@
+import * as constructs from 'constructs';
+import { ICluster } from './cluster';
+import { IComputeNodeGroup } from './compute-node-group';
+import { CfnQueue } from './pcs.generated';
+import * as cdk from '../../core';
+
+/**
+ * Error thrown when an invalid queue ARN is provided
+ */
+class InvalidQueueArnError extends Error {
+  constructor(arn: string) {
+    super(`Invalid queue ARN: ${arn}`);
+    this.name = 'InvalidQueueArnError';
+  }
+}
+
+/**
+ * Configuration for associating a compute node group with a queue
+ */
+export interface ComputeNodeGroupConfiguration {
+  /**
+   * The compute node group to associate with the queue
+   */
+  readonly computeNodeGroup: IComputeNodeGroup;
+}
+
+/**
+ * Properties for defining a PCS Queue
+ */
+export interface QueueProps {
+  /**
+   * The name that identifies the queue
+   *
+   * @default - A name is automatically generated
+   */
+  readonly queueName?: string;
+
+  /**
+   * The cluster this queue belongs to
+   */
+  readonly cluster: ICluster;
+
+  /**
+   * The compute node groups to associate with this queue
+   *
+   * @default - No compute node groups associated
+   */
+  readonly computeNodeGroupConfigurations?: ComputeNodeGroupConfiguration[];
+
+  /**
+   * Tags to apply to the queue
+   *
+   * @default - No tags
+   */
+  readonly tags?: Record<string, string>;
+}
+
+/**
+ * Represents a PCS Queue
+ */
+export interface IQueue extends cdk.IResource {
+  /**
+   * The ARN of the queue
+   * @attribute
+   */
+  readonly queueArn: string;
+
+  /**
+   * The ID of the queue
+   * @attribute
+   */
+  readonly queueId: string;
+
+  /**
+   * The name of the queue
+   * @attribute
+   */
+  readonly queueName: string;
+
+  /**
+   * The cluster this queue belongs to
+   */
+  readonly cluster: ICluster;
+}
+
+/**
+ * Properties for importing an existing queue
+ */
+export interface QueueAttributes {
+  /**
+   * The ARN of the queue
+   */
+  readonly queueArn: string;
+
+  /**
+   * The ID of the queue
+   */
+  readonly queueId: string;
+
+  /**
+   * The name of the queue
+   */
+  readonly queueName: string;
+
+  /**
+   * The cluster this queue belongs to
+   */
+  readonly cluster: ICluster;
+}
+
+/**
+ * A PCS Queue for managing job scheduling in an HPC cluster
+ */
+export class Queue extends cdk.Resource implements IQueue {
+  /**
+   * Import an existing queue by specifying its attributes
+   */
+  public static fromQueueAttributes(scope: constructs.Construct, id: string, attrs: QueueAttributes): IQueue {
+    class Import extends cdk.Resource implements IQueue {
+      public readonly queueArn = attrs.queueArn;
+      public readonly queueId = attrs.queueId;
+      public readonly queueName = attrs.queueName;
+      public readonly cluster = attrs.cluster;
+    }
+
+    return new Import(scope, id);
+  }
+
+  /**
+   * Import an existing queue by its ARN
+   */
+  public static fromQueueArn(scope: constructs.Construct, id: string, queueArn: string): IQueue {
+    const arnParts = cdk.Arn.split(queueArn, cdk.ArnFormat.SLASH_RESOURCE_NAME);
+    const queueId = arnParts.resourceName;
+
+    if (!queueId) {
+      throw new InvalidQueueArnError(queueArn);
+    }
+
+    class Import extends cdk.Resource implements IQueue {
+      public readonly queueArn = queueArn;
+      public readonly queueId = queueId!;
+      public readonly queueName = queueId!;
+      // Create a minimal cluster reference - users should use fromQueueAttributes for full control
+      public readonly cluster: ICluster = {
+        clusterId: 'unknown',
+        clusterArn: 'unknown',
+        clusterName: 'unknown',
+      } as ICluster;
+    }
+
+    return new Import(scope, id);
+  }
+
+  /**
+   * Import an existing queue by its ID
+   */
+  public static fromQueueId(scope: constructs.Construct, id: string, queueId: string): IQueue {
+    const stack = cdk.Stack.of(scope);
+    const queueArn = cdk.Arn.format({
+      service: 'pcs',
+      resource: 'queue',
+      resourceName: queueId,
+    }, stack);
+
+    return Queue.fromQueueArn(scope, id, queueArn);
+  }
+
+  public readonly queueArn: string;
+  public readonly queueId: string;
+  public readonly queueName: string;
+  public readonly cluster: ICluster;
+
+  private readonly cfnQueue: CfnQueue;
+  private readonly computeNodeGroupConfigs: ComputeNodeGroupConfiguration[];
+
+  constructor(scope: constructs.Construct, id: string, props: QueueProps) {
+    super(scope, id);
+
+    this.cluster = props.cluster;
+    this.computeNodeGroupConfigs = props.computeNodeGroupConfigurations || [];
+
+    // Convert compute node group configurations
+    const computeNodeGroupConfigurations = this.computeNodeGroupConfigs.map(config => ({
+      computeNodeGroupId: config.computeNodeGroup.computeNodeGroupId,
+    }));
+
+    // Create the queue
+    this.cfnQueue = new CfnQueue(this, 'Resource', {
+      name: props.queueName,
+      clusterId: this.cluster.clusterId,
+      computeNodeGroupConfigurations: computeNodeGroupConfigurations.length > 0 ? computeNodeGroupConfigurations : undefined,
+      tags: props.tags,
+    });
+
+    this.queueArn = this.cfnQueue.attrArn;
+    this.queueId = this.cfnQueue.attrId;
+    this.queueName = this.cfnQueue.name || this.cfnQueue.attrId;
+  }
+
+  /**
+   * Add a compute node group to this queue
+   */
+  public addComputeNodeGroup(computeNodeGroup: IComputeNodeGroup): void {
+    this.computeNodeGroupConfigs.push({
+      computeNodeGroup,
+    });
+
+    // Update the CloudFormation resource
+    const currentConfigs = this.cfnQueue.computeNodeGroupConfigurations as any[] || [];
+    currentConfigs.push({
+      computeNodeGroupId: computeNodeGroup.computeNodeGroupId,
+    });
+
+    this.cfnQueue.computeNodeGroupConfigurations = currentConfigs;
+  }
+
+  /**
+   * Add tags to the queue
+   */
+  public addTags(tags: Record<string, string>): void {
+    Object.entries(tags).forEach(([key, value]) => {
+      cdk.Tags.of(this).add(key, value);
+    });
+  }
+
+  /**
+   * Get the status of the queue
+   */
+  public get status(): string {
+    return this.cfnQueue.attrStatus;
+  }
+
+  /**
+   * Get error information if the queue failed to provision
+   */
+  public get errorInfo(): cdk.IResolvable {
+    return this.cfnQueue.attrErrorInfo;
+  }
+
+  /**
+   * Get the compute node groups associated with this queue
+   */
+  public get computeNodeGroups(): IComputeNodeGroup[] {
+    return this.computeNodeGroupConfigs.map(config => config.computeNodeGroup);
+  }
+}
