224d842167925ebe33e12eb9908572ce55710bb0
Matthew Murphy <matmur@amazon.com>
2025-07-10 15:07:19 -0700

    Add AWS PCS L2 constructs

--- packages/aws-cdk-lib/aws-pcs/lib/index.ts
+++ packages/aws-cdk-lib/aws-pcs/lib/index.ts
@@ -1,2 +1,11 @@
 // AWS::PCS Cloudformation Resources
 export * from './pcs.generated';
+
+// L2 Constructs
+export * from './cluster';
+export * from './compute-node-group';
+export * from './queue';
+
+// Enums and Helper Classes
+export * from './enums';
+export * from './slurm-configuration';
